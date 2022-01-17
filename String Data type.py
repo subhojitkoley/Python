9c2cc6cc4fd511650efe17027926cc82ba291a6f
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-17 13:19:10 +0530

    6th

---
+++ String Data type.py
@@ -0,0 +1,18 @@
+#String Data type
+# s u b h o j i t     >valu
+# 0 1 2 3 4 5 6 7     >index valu
+
+str="subhojit"
+
+#print(len(str)) / print(str) / print(str[1]) 
+#print(str[5:8])   start:stop
+#print(str[0:5:2]) start:stop:stepover
+#print(str[::])    default
+
+
+
+#print(str[::-1])  "revers print valu"  [::-1] > -1 is "stepover
+# s u b h o j i t
+#-8-7-6-5-4-3-2-1
+
+print(str[::-1])
