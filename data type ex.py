e6b7603cdc2b6d8100841518af500f27a7770882
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-17 13:15:45 +0530

    3rd

---
+++ data type ex.py
@@ -0,0 +1,14 @@
+#data type (int a=10) (float b=7.00) (str c="hello"/c="100")
+
+a=10
+b=5
+c=7.3
+d=5.2
+e="2"
+f="3"
+g=str(5)
+
+#print(a+b) / print(a+c) / print(c*d) / print(e+f)
+#print(int(b*d)) / print(float(a+b)) / 
+
+print(f+g)
