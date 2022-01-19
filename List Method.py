824ddecc11c7cfb77f196293f22059ef05bc9e45
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-19 18:09:48 +0530

    11

---
+++ List Method.py
@@ -0,0 +1,41 @@
+#List Method
+"""
+#ex1
+li=[1,2,3]
+li.append(4)
+print(li)
+
+
+#ex2
+li=[1,2,3]
+li.insert(1   ,    5)
+#.insert(index, object)
+print(li)
+
+
+#ex3
+li=[1,2,3]
+li.pop(1)
+#pop >remove 1 index value
+print(li)
+
+
+#ex4
+li=[1,2,3]
+li.clear()
+#clear li list all value
+print(li)
+
+
+#ex5
+li=[1,2,3,70,10]
+li.sort()
+#sort use to low to high value waise show
+print(li)
+
+"""
+#ex6
+li=[100,20,3,70,10]
+a=li.pop(2)
+#remove 2 index value & put the remove valu under "a" vareable
+print(a)
