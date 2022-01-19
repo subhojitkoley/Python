ffc44210fcf3b62690fa842bd5c3c5f57fdd7335
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-19 18:09:20 +0530

    10

---
+++ List data type.py
@@ -0,0 +1,16 @@
+#List data type
+#li(you can use ane name{ li>is a list name })
+li=["apple","orange","banana"]
+li1=[1,2,3,4]
+li2=["apple",1,True,4]
+
+#"apple" , 1 , True , 4        (list value)
+#   0      1     2    3         (index no.)
+
+
+#print (li2) /print (li) / print (li1)
+
+print (li2)
+
+#li,li1 >is "homogenous/same type of data assine...
+#li2 >is "heterogenous/all type of dataassine..
