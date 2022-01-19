96dbbdec5272065b515b6a07e47032a763a8b7f6
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-19 18:08:21 +0530

    8

---
+++ string Function & methode.py
@@ -0,0 +1,31 @@
+#string Function & methode
+
+#function ex:> print() , type() , int()  len()
+#methode ex:>  .upper() , .lower() , .replace()
+
+
+"""
+function ex>
+print(len("how are you"))
+print(type("how are you"))
+print(int(3.2))
+"""
+
+
+str="Hello Guys"
+print(str . replace("G" , "B"))
+
+"""
+method ex>
+str="Hello Guys"
+
+print(str.upper())
+print(str.lower())
+print(str . replace("G" , "B"))
+
+"""
+
+
+#function is a named selection of a code that performs a specific task..
+
+#method is a function that belong to an object
