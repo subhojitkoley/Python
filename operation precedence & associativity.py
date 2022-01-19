f3fb3f0225cb73126ab3433e3791865cb0fef951
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-19 13:34:52 +0530

    5th

--- operation precedence & associativity.py
+++ operation precedence & associativity.py
@@ -1,18 +1,7 @@
-#String Data type
-# s u b h o j i t     >valu
-# 0 1 2 3 4 5 6 7     >index valu
+#operation precedence & associativity.py
+#rules:> " ()  >  ** >  */  > +-
+#priority>  1  >   2 >   3  >  4
+#other ways left to right calculation.
 
-str="subhojit"
-
-#print(len(str)) / print(str) / print(str[1]) 
-#print(str[5:8])   start:stop
-#print(str[0:5:2]) start:stop:stepover
-#print(str[::])    default
-
-
-
-#print(str[::-1])  "revers print valu"
-# s u b h o j i t
-#-8-7-6-5-4-3-2-1
-
-print(str[::-1])
+#print(3+2*5) / print(2**2+10) /
+print((5+3)*8)
