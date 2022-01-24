4d545c822ec52f01aaf1ce3ee9f81e1dfb5c1d8f
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-24 12:48:24 +0530

    14

---
+++ Dicitionary Method.py
@@ -0,0 +1,15 @@
+#Dicitionary Method
+
+dict={
+      "name":"subhojit",
+      "roll":1
+      }
+#print(dict.keys())          #show all dict key
+#print("name" in dict.keys())     #name key valu is present or not chack
+#print("roll" in dict.keys())     #roll  key valu is present or not chack
+#print(1 in dict.values())        #1 is present or not under the valu chack
+#print("subhojit" in dict.values()) #subhojit is present or not under the valu chack
+#print(dict.items())               #all item show key+value
+
+
+print(dict.clear())            #all clear
