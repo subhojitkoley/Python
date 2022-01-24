5a2756f6b62a40f7b6678610a9d7a4f608379573
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-24 12:48:49 +0530

    15

---
+++ Dicitionary clear copy update Method.py
@@ -0,0 +1,29 @@
+#Dicitionary "clear/copy/update" Method
+"""
+ex>clear
+
+dict={
+      "name":"subhojit",
+      "roll":1
+      }
+dict.clear()
+print(dict) 
+
+
+#ex>copy
+
+dict={
+      "name":"subhojit",
+      "roll":1
+      }
+dic2=dict.copy()
+print(dic2)
+
+"""
+#ex>update     roll 1 to 10
+dict={
+      "name":"subhojit",
+      "roll":1
+      }
+dict.update({"roll": 10})
+print(dict)
