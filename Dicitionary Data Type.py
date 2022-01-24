24e0512fdc990a5bb0c056916f777562f74ae713
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-24 12:48:01 +0530

    13

---
+++ Dicitionary Data Type.py
@@ -0,0 +1,18 @@
+#Dicitionary Data Type
+#a,b,c,d> is key,,,,1,2,3,4,5,True>is value
+
+#key is inmutable & unik....
+#dicitinoary represent as {}
+#list represent as []
+
+dict={
+      "a":1,
+      "b":2,
+      "c":[3,4,5],
+      "d":True
+      }
+
+#print(dict["b"])
+
+#print(dict["c"] [1])  #1 is index no.
+print(dict["d"])
