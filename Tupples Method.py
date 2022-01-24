1361f1ce938148752be4daa204a81a84f4880839
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-24 12:49:48 +0530

    17

---
+++ Tupples Method.py
@@ -0,0 +1,7 @@
+#Tupples Method
+#Tupples is 2 methode 1>.count , 2>.index
+
+tup=(1,2,1,2,3,4,5,4,4,5,4)
+#print(tup.count(2))     #count tup under value 2 is how many time use...
+
+print(tup.index(4))     #index of 4,only 1st porsen index valu show
