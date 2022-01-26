2768c266c148098651164eeb3b01ff9bea83af94
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-26 18:13:16 +0530

    26

---
+++ Logical operators AND.py
@@ -0,0 +1,12 @@
+#Logical operators "and"
+age=17
+Indian=True
+if age>18 and Indian:
+    print("allow to vote")
+else:
+    print("not allow to vote")    
+    
+    
+#age>17   is input-1    
+#and      operator
+#Indian   is input-2
