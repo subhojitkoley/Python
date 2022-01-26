b39f5b73284c22da4fbeaad412600d061b5d3ed1
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-26 18:10:40 +0530

    20

---
+++ Set-data type.py
@@ -0,0 +1,6 @@
+#Set-data type
+#collection of unoded element , or every element is unik
+
+creatset={1,2,2,3,4,5,5,6}
+#print(creatset)             #print every single valu is singel time cz set is unick
+print(len(creatset))         #lenth of the valu,one valu mark is one time
