e2b6291c2164cd7fe60393d1fc21306acbb13bd6
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-26 18:12:54 +0530

    25

---
+++ Logical operators ex.py
@@ -0,0 +1,26 @@
+#Logical operators
+#3 logical operator >1-and ,2-or  ,3-not
+
+ex>and
+
+input-1     input-2       output
+  F           F              F
+  F           T              F
+  T           F              F
+  T           T              T
+  
+  
+ex>or
+
+input-1     input-2        output
+  F           F              F
+  F           T              T
+  T           F              T
+  T           T              T
+  
+  
+ex>not
+
+input      output
+  F           T
+  T           F 
