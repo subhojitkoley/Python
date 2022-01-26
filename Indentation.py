c3a9199919c608a6c592ed612fbf6bbbd6559eea
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-26 18:12:30 +0530

    24

---
+++ Indentation.py
@@ -0,0 +1,8 @@
+#Indentation
+if True:
+    print("hello")
+#__  this space is Indentation, ..use in->if,else,elif
+
+
+#"Indentation" is python refers to the to the(space & tabs) than are use at the beginning of a statement.
+#the statement with the same Indentation belong to the same group called a Suite   
