5894ed689ff87ee0bcd3fc4e7dd8aed7579b843f
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-01-26 18:12:04 +0530

    23

---
+++ Conditional Logic(if,elif,else).py
@@ -0,0 +1,18 @@
+#Conditional Logic (If/Elif/Else)
+
+a=2
+b=30
+c=40
+if a>b:
+    print("a is greaterthan b")
+elif b>c:
+    print("b is greaterthan c")
+else:
+    print("c is greaterthan a&b")
+    
+    
+    
+#elif  ->u can use elif multiple time 
+
+#_____(space>use any conditional)
+#this space is called "indentation"
