f71cb1eb4b4c942d318ef43f99f25ffa885452f9
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-02-01 17:54:49 +0530

    35

---
+++ Enumerate.py
@@ -0,0 +1,19 @@
+#Enumerate
+
+
+#ex>1
+
+for item in enumerate("subhojit"):
+    print(item)                    #show this name with counting number
+    
+
+#ex>2
+for index,item in enumerate("subhojit"):
+    print(index,item)            #show this name  with index value
+
+
+#ex>3
+for index,item in enumerate([10,20,30]):
+    print(index,item)        #you can also use this format
+    
+    
