53a141df5012dcb7fca57213a76d70b67ba76f03
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-02-01 17:52:21 +0530

    30

---
+++ Comparison Operators.py
@@ -0,0 +1,22 @@
+#Comparison Operators
+
+if 5>4:       #5 is big to 4...so its statement right..result>hello
+    print("hello")    
+else:
+    print("welcome")    
+    
+
+
+"""
+note>
+
+> guterthan   ,,  < lessthan    ,,   >= gaterthan equalto  
+<= lessthan equalto   ,, == equalto   ,,  != not equalto
+
+     
+
+ex>you can use-   if ___
+
+5>4 , 5<4 , 5>5 , 5<=5 , 5==5 , 5!=6 , etc
+
+"""
