cfa0e175226d1848972512a5ca5ae18bb2179ef9
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-02-01 17:54:22 +0530

    34

---
+++ Range.py
@@ -0,0 +1,23 @@
+#Range
+
+
+#ex>1
+print(list (range(0,100)))      #1 too 100 print
+
+#ex>2
+for item in range(1,10):         #"item" but you can name anething
+    print(item)                 #print 1 too 9 in colum waise
+    
+#ex>3
+for item in range(1,10):
+    print("hello")             #hello print 9 times
+    
+#ex>4    
+
+for item in range(1,10,2):        #( 1    ,  10  ,    2     )
+    print(item)                   #start    stop    stapover    
+    
+
+#ex>5
+for item in range(10,1,-1):
+    print(item)                #10 too 1 wise show
