ea5f01f57b25e499862d5b4e3b941163a6988cce
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-02-01 17:56:16 +0530

    38

---
+++ Pass.py
@@ -0,0 +1,17 @@
+#Pass
+
+#pass is a "null" statement,,when you can't any logical operation,,so just ignore them when "pass"use
+
+#ex>1
+x=1
+if x==1:
+    pass
+else:
+    print("hello")
+ 
+#ex>2
+x=2
+if x==1:
+    pass
+else:
+    print("hello")
