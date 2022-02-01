60093a51d1f6727a4c402c01c12133c08dabf6de
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-02-01 17:57:03 +0530

    39

---
+++ Input.py
@@ -0,0 +1,24 @@
+#Input    "input is a "str" data type
+
+#ex>1
+x=input("enter a number ")
+print(x)                #user input is show
+
+#ex>2
+x=input("enter a number ")
+print(type(x))          #input data type show
+
+
+#ex>3
+x=int(input("enter a number "))
+print(type(x))    #when u use calculation,,,this time int data you use
+
+#ex>4  (using calculator)
+x=(int(input("enter x number")))
+y=(int(input("enter y number")))
+print(x+y)
+
+#ex>5  (str calculation)
+x=(input("enter x number"))
+y=(input("enter y number"))
+print(x+y)
