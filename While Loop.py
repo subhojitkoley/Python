ebe79ecdb7054d7d9e191dc5d3b88a9043122b92
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-02-01 17:52:43 +0530

    31

---
+++ While Loop.py
@@ -0,0 +1,12 @@
+#While Loop
+"""
+i=0                  #i=0   you can youse anything
+while i<5:
+    print("hello")    #its infinite loop,cz (i<5) its not complite,,
+    
+"""    
+    
+i=0                  #i=0   you can youse anything
+while i<5:
+    print("hello")
+    i=i+1           #hello print 5 time,,cz i+1=1,i(1)1=2,i(2)+1=3...as par as...
