a89ba7ce830dd7c82e74611978d6856033e07761
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-02-01 17:58:36 +0530

    42

---
+++ Return.py
@@ -0,0 +1,25 @@
+#Return   >use "to end the execution of the function call & returns the result
+
+#ex>1
+def add(a,b,c):
+    return a+b+c
+print(add(2,4,6))
+
+
+#ex2>
+def f1():
+    return "welcome"
+print (f1()+" back")
+print(f1()+" home")
+
+
+#ex>3  {f1/add/mul/sub  -- is function name}
+def add(a,b):
+    return a+b
+def mul(a,b):
+    return a*b
+def sub(a,b):
+    return a-b
+print(add(2,3))
+print(mul(2,3))
+print(sub(2,3))
