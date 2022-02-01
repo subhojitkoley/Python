db8e3d3912bfe4bd3045da1e2d68de9df6a7b4a5
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-02-01 17:57:31 +0530

    40

---
+++ Function.py
@@ -0,0 +1,15 @@
+#Function     (function use ->def)
+
+#ex>1   (f1 print one time)
+def f1():       #f1 u can use ane name
+    print("welcome")
+f1()
+
+
+#ex>2   (3 time print)
+def f1():       #f1 u can use ane name  & 3 time print
+    print("welcome")
+f1()
+f1()
+f1()
+
