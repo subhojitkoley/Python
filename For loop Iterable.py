7b8e48aec653494f9cfc521b374a71602dccdb19
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-02-01 17:53:56 +0530

    33

---
+++ For loop Iterable.py
@@ -0,0 +1,26 @@
+#For loop "Iterable"
+
+#Iterable->set,list,tupple,string,dictionary
+#not> int object not iterable
+
+#an iterable is 'any python objectcapable of returing its member one at a time'....
+#permitter it to be iterable over in a for loop..
+
+"""
+
+ex>1
+a=str(123)
+for item in a:
+    print(item)
+    
+"""
+student={
+    'name' :'subhojit' ,
+    'roll' : 1
+    }
+
+for items in student.items():
+    print(items)
+    
+    
+#you can use> .items , .values -
