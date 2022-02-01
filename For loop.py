bb5e86d38265023f80934593c30d0f3ace966eb7
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-02-01 17:53:21 +0530

    32

---
+++ For loop.py
@@ -0,0 +1,10 @@
+#For loop
+"""
+ex>1
+for item in 'hello':
+    print(item)
+    
+"""
+for item in [1,2,3]:
+    for var in "abc":
+        print(item,var)  #item,var  -is vareable,,you can any name...
