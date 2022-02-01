7828d94a7a2d6976d1f2e006a53208525263677b
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-02-01 17:59:42 +0530

    44

---
+++ Ternary Operator.py
@@ -0,0 +1,17 @@
+#Ternary Operator
+#>is a type of 'conditional expression' in python that evaluates a statement
+
+#simplly "multiple line code in one line"
+
+#ex>  if-statement if condition else else-statement
+print("True") if True else print ("false")
+
+#ex1>  if-statement if condition else else-statement
+print("True") if False else print ("false")
+
+#ex2>
+is_allowed="yes" if 5>4 else "no"
+print(is_allowed)
+
+#5>4 -its true--print-is_allowed
+#4>5 -its false --print- no
