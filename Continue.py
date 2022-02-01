153de4fd38ce0551e1bb770068ef3c03fa2fd2b7
subhojit koley <68047312+subhojitkoley@users.noreply.github.com>
2022-02-01 17:55:48 +0530

    37

---
+++ Continue.py
@@ -0,0 +1,8 @@
+#Continue
+
+for item in[10,20,30]:
+    if item==20:
+        continue
+    print(item)      #item is a vareable
+    
+#when line counting and find 20,than skip 20,,and also counting process is running
